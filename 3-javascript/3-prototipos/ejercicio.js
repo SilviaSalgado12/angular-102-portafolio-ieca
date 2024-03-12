70fa4d615a45ba4e1824ed65f4fc4e75141ca176
silvia.salgado <silvia.salgadojuarez@gmail.com>
2024-03-12 13:12:13 -0600

    Subí el archivo de música

---
+++ 3-javascript/3-prototipos/ejercicio.js
@@ -0,0 +1,34 @@
+// AÑADIENDO MIEMBROS A LOS GRUPOS Y MOSTRANDO RESUMEN
+// Completa el código del prototipo "Grupo"
+
+/*
+    1. Utilizando un constructor de objetos "Grupo"
+    2. Completa el constructor "Grupo" con las propiedades:
+        nombre   -> Nombre del grupo        -> String "..."
+        personas -> Arreglo de Personas     -> Array["...", "...", "..."]
+    3. Agrega un prototipo llamado `agregarPersona` que sea una función para agregar una persona
+    4. Agrega un prototipo llamado `mostrarResumen` utilizando **interpolación** para mostrar el siguiente mensaje:
+        "El grupo <propiedad_grupo> tienen <total_personas> miembros"
+*/
+
+const magios = new Grupo("Magios", ["Eduardo", "Carla", "Marcos", "Emmanuel", "Samara", "Paulina", "María", "Julio", "Alex", "Pedro", "Guillermo", "Hernán", "Eliot", "Israel", "Lucía"])
+
+// Constructor de Objetos "Grupo"
+function Grupo(nombre, personas) {
+    const magios = new Grupo("Magios", ["Eduardo", "Carla", "Marcos", "Emmanuel", "Samara", "Paulina", "María", "Julio", "Alex", "Pedro", "Guillermo", "Hernán", "Eliot", "Israel", "Lucía"])
+
+}
+
+function agregarPersona(nombrepersona ){
+    
+    
+    persona.splice(1, 0, '$nombrepersona');
+    // Inserts at index 1
+    console.log(persona);
+
+}
+// magios.mostrarResumen()
+// magios.agregarPersona("Manuel")
+// magios.agregarPersona("Irma")
+// magios.agregarPersona("Carlos")
+// magios.mostrarResumen()
